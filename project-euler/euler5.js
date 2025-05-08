e9827a282d4dc26ebbc84db1f79f6e789564030a
graslowsnail <graslowsnail@gmail.com>
2025-05-07 23:59:27 -0500

    eular 5

---
+++ project-euler/euler5.js
@@ -0,0 +1,22 @@
+// euler 5 
+// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder. 
+// what is the smalled positive number that is EVENLY DIVISIBLE by all numbers from 1 to 20?
+
+
+// for every number 2-20 check if num is evenly divisible, so this function will be used as a helper
+const isDivisable = (num) => {
+  for (let i = 2 ; i <= 20 ; i++) {
+    if (num % i !== 0 ) { 
+      return false;
+    }
+  }
+  return true;
+}
+
+let num = 2
+while (!isDivisable(num)){
+
+  num++
+  console.log(num)
+}
+
