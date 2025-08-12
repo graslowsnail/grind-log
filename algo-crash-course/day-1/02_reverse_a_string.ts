cb28f4a25583ce3f631bafacd70c8383aee953f5
graslowsnail <graslowsnail@gmail.com>
2025-08-12 19:04:40 -0400

    reverse a string

--- algo-crash-course/day-1/02_reverse_a_string.ts
+++ algo-crash-course/day-1/02_reverse_a_string.ts
@@ -5,5 +5,15 @@ Given a string s, return the reverse of s without using built-in reverse helpers
 Input: string s
 Output: string
 */
+const s = 'Pablo Ramirez'
 
+const reverseString = (s: string) => {
+  let stringLength = s.length - 1
+  let reverseString = ''
+  for(let i = stringLength; i >= 0; i--){
+    reverseString = reverseString.concat(s[i])
+  }
+  return reverseString
+}
 
+console.log(reverseString(s))
