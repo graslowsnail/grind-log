e53cf0ae3086c93c3b9e672e48560e8466e6ac7b
graslowsnail <graslowsnail@gmail.com>
2025-08-13 12:10:49 -0400

    solution for counting vowles in string

--- algo-crash-course/day-1/04_count_vowels.ts
+++ algo-crash-course/day-1/04_count_vowels.ts
@@ -5,5 +5,28 @@ Count the number of vowels (a, e, i, o, u) in a given string. Treat uppercase an
 Input: string s
 Output: number
 */
+const test1 = "hello"; // should return 2 (e, o)
+const test2 = "AEIOU"; // should return 5 (all vowels, uppercase)
+const test3 = "rhythm"; // should return 0 (no vowels)
+const test4 = "The quick brown fox jumps over the lazy dog"; // should return 11
 
+const countVowels = (s:string): number => {
+  const vowles = ['a', 'e', 'i','o','u']
+  const wordSet = new Set(vowles)
 
+  let numberOfVowles = 0
+  let cleanedString = s.toLocaleLowerCase()
+
+  for(let i = 0; i< cleanedString.length; i++){
+    if(wordSet.has(cleanedString[i]!)){
+      numberOfVowles = numberOfVowles + 1
+    }
+  }
+  cleanedString.includes
+  return numberOfVowles
+}
+
+console.log(countVowels(test1))// 2 
+console.log(countVowels(test2))// 5
+console.log(countVowels(test3))// 0
+console.log(countVowels(test4))// 11
