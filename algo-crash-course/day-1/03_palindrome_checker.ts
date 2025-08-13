49a17615333903900b80e1c02da39ee9224746c6
graslowsnail <graslowsnail@gmail.com>
2025-08-13 11:38:18 -0400

    is palindrome solution

--- algo-crash-course/day-1/03_palindrome_checker.ts
+++ algo-crash-course/day-1/03_palindrome_checker.ts
@@ -6,4 +6,27 @@ Input: string s
 Output: boolean
 */
 
+// we need to know every value in a string
+const s = 'string.'// false
+const r = 'Maam' // true
+const testPalindromeTrue = "A man, a plan, a canal: Panama"; // should return true
+const testPalindromeFalse = "race a car"; // should return false 
+
+const isPalindrome = (s: string): boolean => {
+  // ai helped with regex again
+  const cleaned = s.replace (/[^a-z0-9]/gi, '').toLowerCase();
+  let reversedS = ''
+  for(let i = cleaned.length; i>= 0; i--){
+    reversedS = reversedS.concat(cleaned[i]?? "")
+  }
+  if(cleaned !== reversedS){
+    return false
+  }
+  return true
+}
+
+console.log(isPalindrome(s))//false
+console.log(isPalindrome(r))//true
+console.log(isPalindrome(testPalindromeFalse))//false
+console.log(isPalindrome(testPalindromeTrue))//true
 
