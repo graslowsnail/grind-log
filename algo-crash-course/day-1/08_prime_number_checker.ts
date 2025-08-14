4f8a84f0b787130b5defdcc4c52bffeb8b130cea
graslowsnail <graslowsnail@gmail.com>
2025-08-14 18:36:44 -0400

    soluton to finding prime numbers

--- algo-crash-course/day-1/08_prime_number_checker.ts
+++ algo-crash-course/day-1/08_prime_number_checker.ts
@@ -5,5 +5,17 @@ Determine whether an integer n is prime. Assume n >= 1.
 Input: number n
 Output: boolean
 */
+// a prime number can only be divided by itself and 1
+const validateIfPrime = (n: number) => {
+  let isPrime = 0
+  for(let i = 1; i<=n; i++){
+    if(n % i === 0){
+      isPrime = isPrime+1
+    }else if(isPrime > 2){
+      return false
+    }
+  }
+  return true
+}
 
-
+console.log(validateIfPrime(25))
